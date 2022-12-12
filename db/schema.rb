72793b3df16fa5d745dc694bb49120c3dbe7e4c5
Omero Arreola <omarreola16@gmail.com>
2022-12-12 21:38:25 +0000

    generate actors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_12_204819) do
+ActiveRecord::Schema.define(version: 2022_12_12_213642) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.date "dob"
+    t.text "bio"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
